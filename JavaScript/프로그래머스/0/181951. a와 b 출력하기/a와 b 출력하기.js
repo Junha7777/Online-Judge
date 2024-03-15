09c5f3d004d693e6e927a901be24e6b7d7b0982f
Junha7777 <130811789+Junha7777@users.noreply.github.com>
2024-03-15 12:06:29 +0900

    [level 0] Title: a와 b 출력하기, Time: 42.74 ms, Memory: 32.2 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.js
+++ JavaScript/프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.js
@@ -9,6 +9,6 @@ let input = [];
 rl.on('line', function (line) {
     input = line.split(' ');
 }).on('close', function () {
-    console.log(`a = ${Number(input[0])}`);
-    console.log(`b = ${Number(input[1])}`);
+    console.log(`a = ${a}`);
+    console.log(`b = ${b}`);
 });
