7b53e95f8760cfb8bfbe11aeb7b2e3e111a3d18f
Junha7777 <130811789+Junha7777@users.noreply.github.com>
2024-05-22 19:32:23 +0900

    [Gold V] Title: 평범한 배낭, Time: 5148 ms, Memory: 229084 KB -BaekjoonHub

---
+++ Python/백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,17 @@
+N, K = map(int, input().split())
+items = []
+for _ in range(N):
+    W, V = map(int, input().split())
+    items.append((W, V))
+
+dp = [[0] * (K+1) for _ in range(N+1)]
+
+for i in range(1, N+1):
+    for j in range(1, K+1):
+        weight, value = items[i-1]
+        if j < weight:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight] + value)
+
+print(dp[N][K])
