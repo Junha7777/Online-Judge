8f4056aca8029a6cc9ae565e54ce5836e965c5d4
Junha7777 <130811789+Junha7777@users.noreply.github.com>
2024-06-06 14:58:52 +0900

    [Bronze I] Title: 초성 중성 종성, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/11284. 초성 중성 종성/초성 중성 종성.py
@@ -0,0 +1,17 @@
+initials = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+medials = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ']
+finals = ['', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+
+char = input().strip()
+
+base_code = ord('가')
+char_code = ord(char) - base_code
+
+initial_index = char_code // (21 * 28)
+medial_index = (char_code % (21 * 28)) // 28
+final_index = char_code % 28
+
+print(initials[initial_index])
+print(medials[medial_index])
+if finals[final_index] != '':
+    print(finals[final_index])
