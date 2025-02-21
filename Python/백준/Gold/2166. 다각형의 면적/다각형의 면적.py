c6d37fa0aa03822971bf091bd9c216e696bcee3c
Junha7777 <130811789+Junha7777@users.noreply.github.com>
2025-02-21 22:26:17 +0900

    [Gold V] Title: 다각형의 면적, Time: 308 ms, Memory: 33432 KB -BaekjoonHub

---
+++ Python/백준/Gold/2166. 다각형의 면적/다각형의 면적.py
@@ -0,0 +1,15 @@
+def shoelace_area(points):
+    n = len(points)
+    area = 0
+
+    for i in range(n):
+        x1, y1 = points[i]
+        x2, y2 = points[(i + 1) % n]
+        area += x1 * y2 - y1 * x2
+
+    return round(abs(area) / 2, 1)
+
+N = int(input())
+points = [tuple(map(int, input().split())) for _ in range(N)]
+
+print(shoelace_area(points))
